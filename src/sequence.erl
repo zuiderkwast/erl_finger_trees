c6174a1eaf1edc1b90d1b1816a7989e2bc9df027
Viktor Söderqvist <viktor@zuiderkwast.se>
2015-01-08 12:17:16 +0100

    Moves examples to src and renames vector to sequence

--- src/sequence.erl
+++ src/sequence.erl
@@ -21,7 +21,7 @@
 %% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 %% POSSIBILITY OF SUCH DAMAGE.
 
--module(vector).
+-module(sequence).
 
 -export([new/0, is_empty/1, size/1,
         pushl/2, peekl/1, popl/1,
@@ -41,73 +41,73 @@ size_op(A, B) ->
     A + B.
 
 new() ->
-    {vector, finger_tree:new(fun size_meas/1, ?size_id, fun size_op/2)}.
+    {sequence, finger_tree:new(fun size_meas/1, ?size_id, fun size_op/2)}.
 
-is_empty({vector, FT}) ->
+is_empty({sequence, FT}) ->
     finger_tree:is_empty(FT).
 
-size({vector, FT}) ->
+size({sequence, FT}) ->
     finger_tree:measure(FT).
 
-pushl(X, {vector, FT}) ->
-    {vector, finger_tree:pushl(X, FT)}.
+pushl(X, {sequence, FT}) ->
+    {sequence, finger_tree:pushl(X, FT)}.
 
-peekl({vector, FT}) ->
+peekl({sequence, FT}) ->
     finger_tree:peekl(FT).
 
-popl({vector, FT}) ->
-    {vector, finger_tree:popl(FT)}.
+popl({sequence, FT}) ->
+    {sequence, finger_tree:popl(FT)}.
 
-pushr(X, {vector, FT}) ->
-    {vector, finger_tree:pushr(X, FT)}.
+pushr(X, {sequence, FT}) ->
+    {sequence, finger_tree:pushr(X, FT)}.
 
-peekr({vector, FT}) ->
+peekr({sequence, FT}) ->
     finger_tree:peekr(FT).
 
-popr({vector, FT}) ->
-    {vector, finger_tree:popr(FT)}.
+popr({sequence, FT}) ->
+    {sequence, finger_tree:popr(FT)}.
 
-concat({vector, FT1}, {vector, FT2}) ->
-    {vector, finger_tree:concat(FT1, FT2)}.
+concat({sequence, FT1}, {sequence, FT2}) ->
+    {sequence, finger_tree:concat(FT1, FT2)}.
 
-subvec(L, H, {vector, FT} = V) when L >= 1, L =< H ->
+subvec(L, H, {sequence, FT} = V) when L >= 1, L =< H ->
     subvec(L, H, finger_tree:measure(FT), V).
 
-subvec(1, Sz, Sz, {vector, FT}) ->
-    {vector, FT};
-subvec(1, H, _Sz, {vector, FT}) ->
-    {vector, finger_tree:takewhile(fun(I) -> I =< H end, FT)};
-subvec(L, Sz, Sz, {vector, FT}) ->
-    {vector, finger_tree:dropwhile(fun(I) -> I < L end, FT)};
-subvec(L, H, _Sz, {vector, FT}) ->
+subvec(1, Sz, Sz, {sequence, FT}) ->
+    {sequence, FT};
+subvec(1, H, _Sz, {sequence, FT}) ->
+    {sequence, finger_tree:takewhile(fun(I) -> I =< H end, FT)};
+subvec(L, Sz, Sz, {sequence, FT}) ->
+    {sequence, finger_tree:dropwhile(fun(I) -> I < L end, FT)};
+subvec(L, H, _Sz, {sequence, FT}) ->
     FT1 = finger_tree:takewhile(fun(I) -> I =< H end, FT),
-    {vector, finger_tree:dropwhile(fun(I) -> I < L end, FT1)}.
+    {sequence, finger_tree:dropwhile(fun(I) -> I < L end, FT1)}.
 
-nth(N, {vector, FT}) ->
+nth(N, {sequence, FT}) ->
     FT1 = finger_tree:dropwhile(fun(I) -> I < N end, FT),
     finger_tree:peekl(FT1).
 
 % insert the element such that it becomes the Nth element
-insert(N, X, {vector, FT}) ->
+insert(N, X, {sequence, FT}) ->
     {L, R1} = finger_tree:split(fun(I) -> I < N end, FT),
     R2 = finger_tree:pushl(X, R1),
-    {vector, finger_tree:concat(L, R2)}.
+    {sequence, finger_tree:concat(L, R2)}.
 
-replace(N, X, {vector, FT}) ->
+replace(N, X, {sequence, FT}) ->
     {L, R1} = finger_tree:split(fun(I) -> I < N end, FT),
     R2 = finger_tree:popl(R1),
     R3 = finger_tree:pushl(X, R2),
-    {vector, finger_tree:concat(L, R3)}.
+    {sequence, finger_tree:concat(L, R3)}.
 
-foldl(Fun, Acc, {vector, FT}) ->
+foldl(Fun, Acc, {sequence, FT}) ->
     finger_tree:foldl(Fun, Acc, FT).
 
-foldr(Fun, Acc, {vector, FT}) ->
+foldr(Fun, Acc, {sequence, FT}) ->
     finger_tree:foldr(Fun, Acc, FT).
 
-to_list({vector, FT}) ->
+to_list({sequence, FT}) ->
     finger_tree:foldr(fun(X, L) -> [X | L] end, [], FT).
 
 from_list(L) ->
-    {vector, lists:foldl(fun finger_tree:pushr/2,
+    {sequence, lists:foldl(fun finger_tree:pushr/2,
             finger_tree:new(fun size_meas/1, ?size_id, fun size_op/2), L)}.
